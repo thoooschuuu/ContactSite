5f8f11b947377d2953be2f79e859d251c11fc383
Thomas Schulze <badbredren@googlemail.com>
2022-06-28 17:48:28 +0200

    closes #2; Localization added

--- site/src/Client/Program.cs
+++ site/src/Client/Program.cs
@@ -1,6 +1,9 @@
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 using TSITSolutions.ContactSite.Client;
+using TSITSolutions.ContactSite.Client.Extensions;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -8,4 +11,11 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-await builder.Build().RunAsync();
+builder.Services.AddMudServices();
+
+builder.Services.AddLocalization();
+builder.Services.AddBlazoredLocalStorage();
+
+var host = builder.Build();
+await host.SetDefaultCulture();
+await host.RunAsync();
