e065462d156858ee4131639c0d0b3a04c4ddb536
Thomas Schulze <badbredren@googlemail.com>
2022-12-10 16:55:33 +0100

    Enable Application Insights for App Service

--- infrastructure/app-service.tf
+++ infrastructure/app-service.tf
@@ -28,9 +28,12 @@ resource "azurerm_linux_web_app" "app" {
   }
 
   app_settings = {
-    WEBSITE_RUN_FROM_PACKAGE       = "1"
-    ASPNETCORE_ENVIRONMENT         = "Production"
-    APPINSIGHTS_INSTRUMENTATIONKEY = azurerm_application_insights.appi.connection_string
-    WEBSITE_WEBDEPLOY_USE_SCM      = "true"
+    WEBSITE_RUN_FROM_PACKAGE                    = "1"
+    ASPNETCORE_ENVIRONMENT                      = "Production"
+    APPLICATIONINSIGHTS_CONNECTION_STRING       = azurerm_application_insights.appi.connection_string
+    WEBSITE_WEBDEPLOY_USE_SCM                   = "true"
+    ApplicationInsightsAgent_EXTENSION_VERSION  = "~3"
+    XDT_MicrosoftApplicationInsights_Mode       = "recommended"
+    XDT_MicrosoftApplicationInsights_PreemptSdk = "1"
   }
 }
