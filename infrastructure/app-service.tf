584988ea05f426e9ddebaa0e1ec9c29dab175002
Thomas Schulze <badbredren@googlemail.com>
2022-12-10 16:40:29 +0100

    Add app setting WEBSITE_WEBDEPLOY_USE_SCM in App Service for contact site

--- infrastructure/app-service.tf
+++ infrastructure/app-service.tf
@@ -1,35 +1,36 @@
 resource "azurerm_service_plan" "plan" {
-    resource_group_name = azurerm_resource_group.rg.name
-    name                = format(local.resource_name_template, local.resource_type_abbreviations["app_service_plan"])
-    location            = local.location
-    os_type             = "Linux"
-    sku_name            = "B1"
+  resource_group_name = azurerm_resource_group.rg.name
+  name                = format(local.resource_name_template, local.resource_type_abbreviations["app_service_plan"])
+  location            = local.location
+  os_type             = "Linux"
+  sku_name            = "B1"
 }
 
 resource "azurerm_linux_web_app" "app" {
-    resource_group_name = azurerm_resource_group.rg.name
-    name                = format(local.resource_name_template, local.resource_type_abbreviations["app_service_environment"])
-    location            = local.location
-    service_plan_id     = azurerm_service_plan.plan.id
-    
-    site_config {
-        ftps_state          = "Disabled"
-        health_check_path   = "/health"
-        http2_enabled       = true
-        use_32_bit_worker   = false
-        
-        application_stack {
-            dotnet_version = "7.0"
-        }
-    }
+  resource_group_name = azurerm_resource_group.rg.name
+  name                = format(local.resource_name_template, local.resource_type_abbreviations["app_service_environment"])
+  location            = local.location
+  service_plan_id     = azurerm_service_plan.plan.id
 
-    identity {
-        type = "SystemAssigned"
-    }
+  site_config {
+    ftps_state        = "Disabled"
+    health_check_path = "/health"
+    http2_enabled     = true
+    use_32_bit_worker = false
 
-    app_settings = {
-      WEBSITE_RUN_FROM_PACKAGE = "1"
-      ASPNETCORE_ENVIRONMENT = "Production"
-      APPINSIGHTS_INSTRUMENTATIONKEY = azurerm_application_insights.appi.connection_string
+    application_stack {
+      dotnet_version = "7.0"
     }
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  app_settings = {
+    WEBSITE_RUN_FROM_PACKAGE       = "1"
+    ASPNETCORE_ENVIRONMENT         = "Production"
+    APPINSIGHTS_INSTRUMENTATIONKEY = azurerm_application_insights.appi.connection_string
+    WEBSITE_WEBDEPLOY_USE_SCM      = "true"
+  }
 }
