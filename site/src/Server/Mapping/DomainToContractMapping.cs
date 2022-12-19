2cdbe07b86639418e1896b3c4c98ee774222abeb
Thomas Schulze <badbredren@googlemail.com>
2022-12-19 21:58:51 +0100

    #5 add markdown capabilities to project description and role

--- site/src/Server/Mapping/DomainToContractMapping.cs
+++ site/src/Server/Mapping/DomainToContractMapping.cs
@@ -1,17 +1,19 @@
-﻿using TSITSolutions.ContactSite.Server.Core;
+﻿using Markdig;
+using TSITSolutions.ContactSite.Server.Core;
 using TSITSolutions.ContactSite.Shared.Projects;
 
 namespace TSITSolutions.ContactSite.Server.Mapping;
 
 internal static class DomainToContractMapping
 {
+    private static readonly MarkdownPipeline _markdownPipeline = new MarkdownPipelineBuilder().UseAdvancedExtensions().Build();
     internal static ProjectResponse ToContract(this Project project) =>
         new()
         {
             Id = project.Id,
             Title = project.Title,
-            Description = project.Description,
-            Role = project.Role,
+            Description = Markdown.ToHtml(project.Description, _markdownPipeline),
+            Role = Markdown.ToHtml(project.Role, _markdownPipeline),
             CustomerDomain = project.CustomerDomain,
             StartDate = project.StartDate,
             EndDate = project.EndDate,
