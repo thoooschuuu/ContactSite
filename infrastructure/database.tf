d3d9f705119a518e2235be0aed97ce09e2747791
Thomas Schulze <badbredren@googlemail.com>
2022-12-23 13:03:18 +0100

    [infrastructure] actually use free tier for cosmos

--- infrastructure/database.tf
+++ infrastructure/database.tf
@@ -16,10 +16,6 @@ resource "azurerm_cosmosdb_account" "db_account" {
     location          = local.location
     failover_priority = 0
   }
-
-  capacity {
-    total_throughput_limit = 400
-  }
 }
 
 resource "azurerm_cosmosdb_mongo_database" "db" {
