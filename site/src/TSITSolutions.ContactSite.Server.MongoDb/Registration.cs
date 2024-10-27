2ba384b8f8c55e67cac0b5bcf7e2ec5cfcd3e6cb
Thomas Schulze <thomas.schulze@thomas-schulze-it-solutions.de>
2024-10-27 12:12:18 +0100

    update formatting

--- site/src/TSITSolutions.ContactSite.Server.MongoDb/Registration.cs
+++ site/src/TSITSolutions.ContactSite.Server.MongoDb/Registration.cs
@@ -8,21 +8,21 @@ using TSITSolutions.ContactSite.Server.Core.Services;
 namespace TSITSolutions.ContactSite.Server.MongoDb;
 
 public static class Registration
-{    
+{
     public static IServiceCollection AddMongoDbStore(this IServiceCollection services, IConfiguration configuration)
     {
         services.Configure<MongoDbOptions>(configuration.GetSection("ProjectsDatabase"));
         services.AddSingleton<IProjectRepository, MongoDbProjectRepository>();
         return services;
     }
-    
+
     public static void InitializeMongoDbStore(this IHost app)
     {
         var options = app.Services.GetRequiredService<IOptionsMonitor<MongoDbOptions>>().CurrentValue;
         var client = new MongoClient(options.ConnectionString);
         var database = client.GetDatabase(options.DatabaseName);
         var collections = database.ListCollectionNames().ToList();
-        
+
         foreach (var collection in MongoSpecs.Collections)
         {
             if (!collections.Contains(collection))
